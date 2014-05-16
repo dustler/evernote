7e26088196de6c357268498150ef7a906c30cd9f
Vasiliy Tyubek <v.tyubek@gmail.com>
2014-05-16 16:02:01 +0400

    Refactoring

--- bin/writer.php
+++ bin/writer.php
@@ -3,38 +3,10 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $application = new VT\Worker\Application();
+
 $application->register(new \VT\Provider\EvernoteServiceProvider());
 
-/*$channel = new AMQPChannel($app['amqp.connection']);
-
-$queue = new AMQPQueue($channel);
-$queue->setName('evernote');
-$queue->setFlags(AMQP_DURABLE);
-$queue->declareQueue();
-
-$queue->consume(function ($message, $queue) use (&$app) {
-    $messageBody = $message->getBody();
-    $json        = json_decode($messageBody, true);
-    $store       = $app['evernote.note_store'];
-
-    $payload = $json['payload'];
-    if (is_array($payload)) {
-        $payload = implode($payload);
-    }
-
-    $nBody = '<?xml version="1.0" encoding="UTF-8"?>';
-    $nBody .= '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">';
-    $nBody .= '<en-note>' . $payload . '</en-note>';
-
-    $note               = new Note();
-    $note->notebookGuid = '';
-    $note->content      = $nBody;
-    $note->title        = $json['title'];
-
-    try {
-        $newnote = $store->createNote($note);
-    } catch (Exception $e) {
-        var_dump($e);
-    }
-    $queue->ack($message->getDeliveryTag());
-});*/
+$application['handler'] = function ($app) {
+    return new Dustler\Evernote\Worker($app);
+};
+$application->run();
--- src/Dustler/Evernote/Worker.php
+++ src/Dustler/Evernote/Worker.php
@@ -8,19 +8,58 @@ use VT\Worker\WorkerInterface;
 
 class Worker implements WorkerInterface
 {
+    protected $application;
+
     /**
      * @param Application $app
      */
-    function __construct(Application $app)
+    public function __construct(Application $app)
     {
-        // TODO: Implement __construct() method.
+        $this->application = $app;
     }
 
     /**
      * @return mixed
      */
-    function handle()
+    public function handle()
     {
-        // TODO: Implement handle() method.
+        $connection = new AMQPConnection();
+        $connection->connect();
+
+        $channel = new AMQPChannel($connection);
+
+        $queue = new AMQPQueue($channel);
+        $queue->setName('evernote');
+        $queue->setFlags(AMQP_DURABLE);
+        $queue->declareQueue();
+
+        $app = $this->application;
+
+        $queue->consume(function ($message, $queue) use (&$app) {
+            $messageBody = $message->getBody();
+            $json        = json_decode($messageBody, true);
+            $store       = $app['evernote.client']->getNoteStore();
+
+            $payload = $json['payload'];
+            if (is_array($payload)) {
+                $payload = implode($payload);
+            }
+
+            $nBody = '<?xml version="1.0" encoding="UTF-8"?>';
+            $nBody .= '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">';
+            $nBody .= '<en-note>' . $payload . '</en-note>';
+
+            $note               = new Note();
+            $note->notebookGuid = '';
+            $note->content      = $nBody;
+            $note->title        = $json['title'];
+
+            try {
+                $newnote = $store->createNote($note);
+            } catch (Exception $e) {
+                var_dump($e);
+            }
+            $queue->ack($message->getDeliveryTag());
+        });
     }
 }
