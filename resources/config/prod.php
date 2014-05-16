4e30415f5ea0f650f2e826b38de4dcd130487314
Vasiliy Tyubek <v.tyubek@gmail.com>
2014-05-16 16:10:22 +0400

    Изменил поведение web приложения

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -1,14 +1,4 @@
 <?php
 
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\FormServiceProvider;
-
 $app['locale'] = 'en';
 $app['debug']  = true;
-
-$app->register(new FormServiceProvider());
-
-$app->register(new TwigServiceProvider(), array(
-    'twig.path' => array(__DIR__.'/views'),
-    'twig.options' => array('cache' => __DIR__.'/../cache/twig'),
-));
--- src/controllers.php
+++ src/controllers.php
@@ -1,15 +1,10 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Response;
-
-use EDAM\Types\Data, EDAM\Types\Note, EDAM\Types\Resource, EDAM\Types\ResourceAttributes;
-use EDAM\Error\EDAMUserException, EDAM\Error\EDAMErrorCode;
-use EDAM\NoteStore\NoteFilter;
-
-$app->match('/note/create/{userid}/{notebookid}/{noteid}', function ($userid = 1, $notebookid = 1, $noteid = 1) use ($app) {
-    $note    = $app['evernote.note_store']->getNote($noteid, true, false, false, false);
-    $message = array('title' => $note->title, 'payload' => $note->content);
-    $app['amqp.exchange']->publish(json_encode($message), 'from_evernote');
+$app->match('*', function (/*$userid = 1, $notebookid = 1, $noteid = 1*/) use ($app) {
+    $message = array('title'   => '',
+                     'payload' => array(/*'userId' => $userid, 'notebookid' => $notebookid, 'noteId' => $noteid*/)
+    );
+    $app['amqp.exchange']->publish(json_encode($message), 'evernote');
 
     return new Response('OK', 200);
 });
